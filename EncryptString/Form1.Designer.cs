5c4465e0198cbb9f72643fbbe2d9f704b1f71253
ArkadyCH <arkadychu4vaga@gmail.com>
2018-12-17 23:23:25 +0300

    first commit

---
+++ EncryptString/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Collections.Generic;
+
+namespace EncryptString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        //Конвертирует строку в двоичное представление
+        static string TextToBinary(string input, System.Text.Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = System.Text.Encoding.UTF8;
+
+            var builder = new System.Text.StringBuilder();
+            var bytes = encoding.GetBytes(input); // Конвертируем текст в байты используя кодировку
+
+            foreach (var b in bytes)
+                builder.Append(Convert.ToString(b, 2).PadLeft(8, '0')); //Конвертируем байты в двоичное представление
+
+            return builder.ToString();
+        }
+        //Конвертируем двоичное представление в строку
+        static string BinaryToText(string bytes, System.Text.Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = System.Text.Encoding.UTF8;
+
+            var byteCount = 8;
+            var byteArray = new byte[bytes.Length / 8]; // Массив для байтов
+            for (int i = 0; i < bytes.Length / byteCount; i++)
+            {
+                var subBytes = bytes.Substring(i * byteCount, byteCount); // Получаем подстроку размером из восьми бит
+                var b = Convert.ToByte(subBytes.TrimStart('0'), 2); // Конвертируем подстроку в байты
+                byteArray[i] = b; // Добавляем байт в массив
+            }
+
+            return encoding.GetString(byteArray); // Конвертируем массив в текст используя выбранный кодировщик
+        }
+        // Зашифровываем строку в байты
+        static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Проверяем наличие проверяемых аргументов
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            // Создаем Rijndael объект
+            // С использованием указанного ключа и вектором инициализации.
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Создаём шифратор для выполнения преобразования потока.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Создаём поток используемый для шифрования.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Записываем все данные в поток.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Возвращаем зашифрованные байты из потока памяти.
+            return encrypted;
+
+        }
+        // Расшифровываем байты в строку
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Проверяем аргументы.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Объявляем строку, которая будет использоваться
+            // для расшифрованого текста.
+            string plaintext = null;
+
+            // Создаем Rijndael объект
+            // С использованием указанного ключа и вектором инициализации.
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+
+                // Создаём дешифратор для выполнения преобразования потока.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Создаём поток используемый для дешифрования.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            // Читаем все дешифрованные байты из дешифрованного потока
+                            // и кладём их в строку.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+            return plaintext;
+        }
+        
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 49);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(42, 173);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(260, 49);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(347, 173);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(260, 49);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(347, 52);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(260, 49);
+            this.textBox6.TabIndex = 4;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(42, 268);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(562, 22);
+            this.textBox7.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Исходный текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Шифр";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Шифр";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(350, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Исходный текст";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Ключ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 381);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ EncryptString/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptString
+{
+    public partial class Form1 : Form
+    {
+        // Создаём вектор инициализации
+        byte[] iv = Convert.FromBase64String("v2Lat7Oxru5CnDSIbfAT / w ==");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+            // Получаем значение из поля "Исходный текст"
+            string text = textBox1.Text;
+            // Конвертируем текст в двоичное представление
+            string bites = TextToBinary(text);
+
+            // Создаём объект Rijndael
+            using (Rijndael myRijndael = Rijndael.Create())
+            {
+                // Если мы не задали ключ, то генерируем Base64 ключ
+                // иначе берём его из поля
+                if (textBox7.Text != "")
+                    myRijndael.Key = Convert.FromBase64String(textBox7.Text);
+                else
+                    myRijndael.GenerateKey();
+                // Конвертируем ключ шифрования в Base64
+                textBox7.Text = Convert.ToBase64String(myRijndael.Key);
+                // Шифруем биты в байты
+                byte[] bytes = EncryptStringToBytes(bites, myRijndael.Key, iv);
+                // Конверитуем зашифрованные байты в Base64
+                string encrypted = Convert.ToBase64String(bytes);
+                // Заносим Base64 в поля "Шифр"
+                textBox3.Text = encrypted;
+                textBox6.Text = encrypted;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Получаем ключ шифрования
+            string textKey = textBox7.Text;
+            // Получаем значение из поля шифр и убираем пробелы
+            string encrypted = textBox6.Text.Replace(" ", "");
+            // Конвертируем зашифрованные Base64 в зашифрованные байты
+            byte[] bytes = Convert.FromBase64String(encrypted);
+            // Дешифруем байты в биты
+            string roundtrip = DecryptStringFromBytes(bytes, Convert.FromBase64String(textKey), iv);
+            // Преобразовываем биты в текст
+            string text = BinaryToText(roundtrip);
+            // Записываем биты в поле "Исходный текст"
+            textBox4.Text = text;
+        }
+    }
+}
